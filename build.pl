be6b00f55e081cdafa5712dd6da92565b99b7467
Andrew Rodland <arodland@shutterstock.com>
2013-12-13 17:10:11 -0500

    Exclude core modules from the fatpack

--- build.pl
+++ build.pl
@@ -2,6 +2,7 @@
 use strict;
 use File::pushd;
 use File::Find;
+use Module::CoreList;
 
 sub generate_file {
     my($base, $target, $fatpack, $shebang_replace) = @_;
@@ -25,7 +26,24 @@ sub generate_file {
     rename "$target.tmp", $target;
 }
 
+sub pm_to_mod {
+    local $_ = shift;
+    s!/!::!g;
+    s/\.pm$//;
+    $_
+}
+
 system('fatpack trace bin/task');
+
+{
+    local @ARGV = ('fatpacker.trace');
+    local $^I = ".bak";
+    while (<>) {
+        chomp;
+        print "$_\n" unless Module::CoreList::is_core(pm_to_mod($_));
+    }
+}
+
 system('fatpack packlists-for $(cat fatpacker.trace) >> packlists');
 
 # add some stuff to the fatlib to get Moo to fatpack
