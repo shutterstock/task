3f78e82659980975a2f50b71503ec495f3005a06
Andrew Rodland <arodland@shutterstock.com>
2013-12-12 19:34:31 -0500

    Try build pushing

---
+++ push_build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+function error_exit
+{
+  echo "$*" 1>&2
+  exit 1
+}
+
+cp task $HOME/task
+
+cd ..
+git clone -q -b gh-pages https://${GH_TOKEN}@github.com/shutterstock/task.git gh-pages >/dev/null 2>&1 || error_exit "Error cloning gh-pages"
+cd gh-pages
+
+git config user.name "Travis Automated Build"
+git config user.email "task@shutterstock.com"
+
+BUILDDIR="build/perl-${TRAVIS_PERL_VERSION}"
+mkdir -p $BUILDDIR/task
+cp $HOME/task $BUILDDIR/task
+git add $BUILDDIR/task
+git commit -q -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push -fq origin gh-pages >/dev/null 2>&1 || error_exit "Error pushing build to github"
